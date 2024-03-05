b7589a17d1c6ce867abe94ee4100309ebbe7ea72
evan-hatley <143963975+evan-hatley@users.noreply.github.com>
2024-03-04 21:11:40 -0600

    Update typeDefs.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -32,6 +32,10 @@ const typeDefs = `
   type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
+    setUserUnavailableDays(username: String!, unavailableDays: [String!]!): User
+    createGroup(groupName: String!, groupCreator: String!): Group
+    addGroupMember(groupId: ID!, username: String!): Group
+
   }
    `;
 
