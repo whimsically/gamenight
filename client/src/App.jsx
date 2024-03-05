f4ad3a4994a92e3fd435aae307a8a0218e99c4ab
jxdang1 <dangjennifer21@gmail.com>
2024-03-04 21:23:40 -0600

    updated main.jsx, created routes.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,44 @@
-import './App.css';
-import Profile  from './pages/Profile';
-import Chats from './pages/Chats';
-import Players from './pages/Players';
-import Games from './pages/Games';
-import Scheduler from './pages/Scheduler';
+import { useLocation, useOutlet, } from 'react-router-dom';
+import { routes } from './routes.jsx'
+
+//components
+import Header from './components/Navigation/Header.jsx'
+import Footer from './components/Footer'
+import Navbar from './components/Navigation/Navigation.jsx';
+
+//styling
+import { CSSTransition, SwitchTransition } from 'react-transition-group'
+import './App.css'
 
 function App() {
+  const location = useLocation()
+  const currentOutlet = useOutlet()
+  const { nodeRef } =
+    routes.find((route) => route.path === location.pathname) ?? {}
   return (
-    <Router>
-      <div>
-        <Header />
-        <Routes>
-          <Route path="/Profile" element={<Profile />} />
-          <Route path="/Chats" element={<Chats />} />
-          <Route path="/Players" element={<Players />} />
-          <Route path="/Games" element={<Games />} />
-          <Route path="/Scheduler" element={<Scheduler />} />
-        </Routes>
-      </div>
+    <>
+      <Header />
+      <main>
+      <Navbar />
+        <SwitchTransition>
+          <CSSTransition
+            key={location.pathname}
+            nodeRef={nodeRef}
+            timeout={300}
+            classNames="page"
+            unmountOnExit
+          >
+            {(state) => (
+              <div ref={nodeRef} className="page">
+                {currentOutlet}
+              </div>
+            )}
+          </CSSTransition>
+        </SwitchTransition>
+      </main>
       <Footer />
-    </Router>
-  );
+    </>
+  )
 }
 
 export default App;
