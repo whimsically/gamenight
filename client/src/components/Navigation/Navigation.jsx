2f14e5d0508d52ace811329547d2ed7fac57026e
MayaH2000 <101356128+MayaH2000@users.noreply.github.com>
2024-03-04 19:50:13 -0600

    created scheduler file, installed dependencies, imported scheduler in index.jsx, created login and signup files

--- client/src/components/Navigation/Navigation.jsx
+++ client/src/components/Navigation/Navigation.jsx
@@ -4,7 +4,7 @@ function Navigation() {
   return (
     <nav className='nav'>
       <ul>
-        <button><NavLink to="/Calender" >Calender</NavLink></button>
+        <button><NavLink to="/Scheduler" >Scheduler</NavLink></button>
         <button><NavLink to="/Chats" >Chats</NavLink></button>
         <button><NavLink to="/Games" >Games</NavLink></button>
         <button><NavLink to="/Players" >Players</NavLink></button>
