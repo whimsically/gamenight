d97a301a6152dd159eaba8dc132ac476e41bcdfe
MayaH2000 <101356128+MayaH2000@users.noreply.github.com>
2024-03-05 20:32:09 -0600

    installed socket.io, created chatroom client side code in chats.jsx, added functionality for changes in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,44 @@
-// import { useState } from 'react';
+import { useState } from 'react';
+import io from 'socket.io-client';
 import { Layout } from 'antd';
 import Logo from './components/Logo';
 import MenuList from './components/MenuList';
-
+import Chat from './pages/Chats';
+import './index.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const { Sider } = Layout;
+const socket = io.connect('http://localhost:4000'); //  our server will run on port 4000, so we connect to it from here
 
 function App() {
+  const [username, setUsername] = useState(''); 
+  const [room, setRoom] = useState(''); 
+
   return (
-    <>
-      <Layout>
+    <Layout>
       <Sider width={300} className="sidebar">
         <Logo />
         <MenuList />
-    </Sider>
+      </Sider>
+      <Router>
+      <div className='App'>
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <Chat
+                username={username} 
+                setUsername={setUsername} 
+                room={room} 
+                setRoom={setRoom} 
+                socket={socket} 
+              />
+            }
+          />
+        </Routes>
+      </div>
+      </Router>
     </Layout>
-    </>
   );
 }
 
