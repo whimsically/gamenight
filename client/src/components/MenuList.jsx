a1ff5842e4f13039afc36582cbdf58016ec73d50
jxdang1 <dangjennifer21@gmail.com>
2024-03-05 19:11:33 -0600

    updated list and added icons

--- client/src/components/MenuList.jsx
+++ client/src/components/MenuList.jsx
@@ -1,7 +1,10 @@
 import { Menu } from 'antd';
 import { 
     HomeOutlined,
+    CalendarOutlined,
+    WechatOutlined,
     UserOutlined,
+    ProfileOutlined,
     SettingOutlined 
 } from '@ant-design/icons';
 
@@ -11,11 +14,17 @@ const MenuList = () => {
             <Menu.Item key="home" icon={<HomeOutlined />}>
                 Home
             </Menu.Item>
-            <Menu.Item key="calendar" >
-                Calendar
+            <Menu.Item key="schedule" icon={<CalendarOutlined />} >
+                Schedule
+            </Menu.Item>
+            <Menu.Item key="chats" icon={<WechatOutlined/>} >
+                Chats
             </Menu.Item>
             <Menu.Item key="players" icon={<UserOutlined />}>
                 Players
+            </Menu.Item>
+            <Menu.Item key="profile" icon={<ProfileOutlined/>}>
+                Profile
             </Menu.Item>
             <Menu.Item key="setting" icon={<SettingOutlined />}>
                 Setting
